baa0bafb5a4c29aa04eed550d47291771ad941fb
Thamer Ayachi <thamerjak3@gmail.com>
2022-11-08 13:17:51 +0100

    add methods.c, main.h and .gitignore

--- main.h
+++ main.h
@@ -5,20 +5,29 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+
+/**
+ * getNumberFormat - number %
+ *
+ * @str: string get in printf
+ *
+ * description: 'get number of % after them character'
+ *
+ */
+int getNumberFormat(char *str);
+
 typedef struct op
 {
-	char p ;  
-	void (*f)(va_list); 
-}t_type;
-int _print(const char * format); 
-int get_printf(const char s,va_list fn);
+	char p;
+	void (*f)(va_list);
+} t_type;
+int _print(const char *format);
+int get_printf(const char s, va_list fn);
 
-void print_int(va_list args) ;
+void print_int(va_list args);
 void print_char(va_list args);
 void print_string(va_list args);
 void print_float(va_list args);
-void get_function(const char *format,...);
-
-
+void get_function(const char *format, ...);
 
 #endif
