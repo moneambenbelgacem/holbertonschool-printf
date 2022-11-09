8a67f2cc6ece9d93b6602079f97f4e1052ce737a
ThamerAyachi <thamerjak3@gmail.com>
2022-11-09 12:21:54 +0100

    add print_unsigned

--- mrthods.c
+++ mrthods.c
@@ -76,3 +76,40 @@ int print_char(va_list v)
 	putchar(c);
 	return (1);
 }
+
+int print_unsigned(va_list v)
+{
+	int n = va_arg(v, int);
+	int num, last = n % 10, digit, exp = 1, i = 1;
+
+	n /= 10;
+	num = n;
+
+	if (last < 0)
+	{
+		putchar('-');
+		num *= (-1);
+		n *= (-1);
+		last *= (-1);
+		i++;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp *= 10;
+			num /= 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			putchar(digit + '0');
+			num -= (digit * exp);
+			exp /= 10;
+			i++;
+		}
+	}
+	putchar(last + '0');
+	return (i);
+}
