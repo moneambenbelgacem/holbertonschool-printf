f7fdfec39d98cbadbe63f9f5efb196b8c17f5e7c
ThamerAyachi <thamerjak3@gmail.com>
2022-11-09 12:37:49 +0100

    add print_pointer and print_hex_aux

--- methods.c
+++ methods.c
@@ -9,6 +9,36 @@ int _strlen(char *s)
 	return (c);
 }
 
+int print_hex_aux(unsigned int num)
+{
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int temp = num;
+
+	while (num / 16 != 0)
+	{
+		num /= 16;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = temp % 16;
+		temp /= 16;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		if (array[i] > 9)
+			array[i] = array[i] + 7;
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
+}
+
 int print_string(va_list v)
 {
 	char *s;
